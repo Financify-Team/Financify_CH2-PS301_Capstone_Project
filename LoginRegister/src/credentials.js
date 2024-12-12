58ec558f16776e0f930499f2109723c8c6041b67
FIK3021 <157036535+FIK3021@users.noreply.github.com>
2024-12-13 01:04:23 +0700

    Update credentials.js

--- LoginRegister/src/credentials.js
+++ LoginRegister/src/credentials.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEBgmyAVQE37AB1fABJLm1CJn5mih1i-Y",
-  authDomain: "capstone--c242-ps301.firebaseapp.com",
-  projectId: "capstone--c242-ps301",
-  storageBucket: "capstone--c242-ps301.firebasestorage.app",
-  messagingSenderId: "599704937134",
-  appId: "1:599704937134:web:760e9d11cb02d5532f833f",
-  measurementId: "G-WVEMQE53EE"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
